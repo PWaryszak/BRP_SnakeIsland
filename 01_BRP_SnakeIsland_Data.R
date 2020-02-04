d767de2f0b0ec6791d1ef319663fbd30d20105d3
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2020-02-04 12:41:09 +1100

    Snake Island Core and Elevation Data

---
+++ 01_BRP_SnakeIsland_Data.R
@@ -0,0 +1,32 @@
+#Install 2 R-packages we need if not on your comp by now:
+if (!require(readxl)) library(readxl) # load that package if not done already
+if (!require(tidyverse)) library(tidyverse) # load that package if not done already
+
+#Load these packages into your working environment
+library("readxl")
+library("tidyverse")
+#setwd("~/Documents/0.DeakinUni/RTK")#set working directory (where you store your data)
+
+snake_core <- read.csv("SnakeIsland_Core.csv")
+snake_elev <- read.csv("SnakeIsland_Elev.csv")
+
+#Merge core and elevation date from above:
+
+snake_data <- left_join(snake_core, snake_elev, by = "SampleID")
+View(snake_data)
+
+#GGPLOT elev versus latitude=====
+
+ggplot(snake_data,aes(x=latitude, y=elevation, color = site, shape = site))+
+  #labs(x = "",y="",colour="year type")+
+  geom_point(size = 3)+
+  facet_grid(.~site,scale="fixed") +
+  theme_bw()+
+    coord_flip()+
+    theme(axis.text.x=element_text(vjust=0.5,size=12),
+        axis.text.y=element_text(size=12),
+        axis.title.y=element_text(size=24),
+        axis.title.x=element_text(size=24),
+        legend.position = "none",
+        strip.text=element_text(size=24))
+
